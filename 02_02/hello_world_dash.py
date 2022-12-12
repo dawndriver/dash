dc5dcba55e656b072f52bc4f25fac65929d604d1
shich <shichendawn@gmail.com>
2022-12-12 14:32:53 -0600

    update

--- 02_02/hello_world_dash.py
+++ 02_02/hello_world_dash.py
@@ -1,5 +1,11 @@
 import dash
 from dash import html
+from dash import dcc
+import plotly.express as px
+import pandas as pd
+
+data = pd.read_csv("precious_metals_prices_2018_2021.csv", usecols=["DateTime", "Gold"])
+fig = px.line(data, x="DateTime", y="Gold", title="Precious Metal Prices 2018-2021")
 
 app = dash.Dash(__name__)
 
